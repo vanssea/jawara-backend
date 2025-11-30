a617a983b454ce3e937a53000d16074b60ba3c7d
Fahreza <fahreza200412@gmail.com>
2025-11-30 10:36:05 +0700

    feat: implementasi pemasukkan kategori iuran module with CRUD oprations and DOTs

---
+++ src/income/pemasukkan_kategori/pemasukkan_kategori_iuran.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { PemasukanKategoriIuranService } from './pemasukkan_kategori_iuran.service';
+import { PemasukanKategoriIuranController } from './pemasukkan_kategori_iuran.controller';
+import { CommonModule } from 'src/common/common.module';
+
+@Module({
+  imports: [CommonModule],
+  controllers: [PemasukanKategoriIuranController],
+  providers: [PemasukanKategoriIuranService],
+})
+export class ExpensesModule {}
